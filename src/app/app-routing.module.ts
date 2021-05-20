bd8d566ac640eade04498d0ef5d9b1c673760d1b
hanksbonniec <hanksbonniec@gmail.com>
2021-05-20 09:33:40 -0700

    Made HeroDetailComponent routable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core'; //initializes router and starts it listening for browser location changes
 import { RouterModule, Routes } from '@angular/router';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent },
   {path: 'dashboard', component: DashboardComponent },
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'detail/:id', component: HeroDetailComponent} //The colon (:) in the path indicates that :id is a placeholder for a specific hero id
 ];
 
 @NgModule({
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,9 +13,23 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, //holds information about the route to this instance of the HeroDetailComponent
+    private heroService: HeroService, //gets hero data from the remote server and this component will use it to get the hero-to-display
+    private location: Location //Angular service for interacting with the browser
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back(); //navigates backward one step in the browser's history stack using the Location service
   }
 
 }
