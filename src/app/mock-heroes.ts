3ec83393c88c04ed0186a12dce275f1b61cb0304
hanksbonniec <hanksbonniec@gmail.com>
2021-05-19 20:51:55 -0700

    Created mock heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Magenta'},
+  {id: 12, name: 'Professor Y'},
+  {id: 13, name: 'Bombardier'},
+  {id: 14, name: 'Mighty Man'},
+  {id: 15, name: 'White Widower'},
+  {id: 16, name: 'Dr OK'},
+  {id: 17, name: 'Orange Orc'},
+  {id: 18, name: 'Poison'},
+  {id: 19, name: 'Pinguino'},
+  {id: 20, name: 'Brown Javelin'}
+
+];
