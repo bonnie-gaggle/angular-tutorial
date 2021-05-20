eac8804dbf5e5a3ca008870ab2d5e2a89d4135ca
hanksbonniec <hanksbonniec@gmail.com>
2021-05-20 08:12:27 -0700

    create and bind message component and service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,9 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
